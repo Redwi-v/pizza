c1c1c5bc086dc8aff1b3e4b8a239553e99b06ad3
Redwi_v <forest45456@gmail.com>
2023-01-04 19:54:29 +0300

    item_refactor

--- src/components/PizzaItem/PizzaItem.tsx
+++ src/components/PizzaItem/PizzaItem.tsx
@@ -19,59 +19,24 @@ const PizzaItem: FC<PizzaItemProps> = (props) => {
 
 
   //logic
-  const [choosedSize , setChoosedSize] = React.useState<number>(pizzaSizes[0])
+  const [selectedSize , setSelectedSize] = React.useState<number | null>(null)
+  const [selectedDought , setSelectedDought] = React.useState<number | null>(null)
 
+  
 
-
-
-  // additional renders
+  // --additional renders
   const renderPizzaSizes = (): React.ReactNode => {
-    
-
     return pizzaSizes.map((pizzaSize, index) => {
-      let thisSizeAvailable: boolean = false
-      let isChoosedCssClass: string =  ''
-
-      thisSizeAvailable =  sizes.indexOf(pizzaSize) === -1 ? false : true
-      isChoosedCssClass = (pizzaSize === choosedSize) && thisSizeAvailable ? style.choosedElement : ''
-
-      
-
-      const isAvailableChooseClass = thisSizeAvailable ? style.available : ''
-
-      const setSize = () => {
-        if(thisSizeAvailable) setChoosedSize(pizzaSize)
-        
-      }
-
-
-      return (
-        <li key={pizzaSize}>
-          <button onClick={setSize} className={`${style.settings_button} ${isAvailableChooseClass} ${isChoosedCssClass} ui_element`}>
-            {pizzaSize}
-          </button>
-        </li>
-      )
+      return <Select text={pizzaSize} availableItems={sizes} item={pizzaSize} selectedItem={selectedSize} setSelectedItem={setSelectedSize} key={index}/>
     })
   }
 
   const renderPizzaDoughs = (): React.ReactNode => {
     return pizza_doughs.map((dough, doughsIndex) => {
-      let thisPizzaAvailable: boolean = false
 
-      types.forEach(index => {
-        if (index === doughsIndex) {
-          thisPizzaAvailable = true
-        }
-      })
+      
 
-      return (
-        <li key={doughsIndex}>
-          <button className={`${style.settings_button} ${thisPizzaAvailable ? style.available : ''} ui_element`}>
-            {dough}
-          </button>
-        </li>
-      )
+      return <Select text={dough} availableItems={types} item={doughsIndex} selectedItem={selectedDought} setSelectedItem={setSelectedDought} key={doughsIndex} />
     })
   }
 
@@ -105,6 +70,55 @@ const PizzaItem: FC<PizzaItemProps> = (props) => {
     </div>
   );
 }
+
+
+//helper 
+interface SelectProps {
+  availableItems: Array<number | string> // items приходящие к нам с объектом (для проверки лежит ли в этом масиве item)
+  item: number | string // нужно для сравнения и хранения (может принимать значение поля text)
+  selectedItem: string | number | null
+  text: string | number // то что будет выводится  
+  setSelectedItem: (value: any) => void
+}
+
+const Select: FC<SelectProps> = ({availableItems, item, text,  selectedItem, setSelectedItem}) => {
+
+  // задаем начальное значение (первый существующий элемент)
+  React.useEffect(() => {
+    setSelectedItem((prevItem: any) => {
+      if(prevItem === null && isAvailable) {
+        return item
+      }
+      return prevItem
+    })
+  }, [])
+
+
+  
+  
+  const isAvailable = availableItems.indexOf(item) === -1 ? false : true
+  const isSelected = selectedItem === item
+   
+
+  const availableClass = isAvailable ? style.available : ''
+  const selectedClass = isSelected ? style.selectedElement : ''
+  
+
+
+
+  const setSize = () => {
+    if(isAvailable) setSelectedItem(item)
+  }
+
+  return (
+    <li>
+      <button onClick={setSize} className={`${style.settings_button} ${availableClass} ${selectedClass} ui_element`}>
+        {text}
+      </button>
+    </li>
+  )
+}
+
 
 
 
